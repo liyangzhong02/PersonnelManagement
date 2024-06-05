4e6404258152cf5d862c197e0f6d30137e542480
marRE <82664059+liyangzhong02@users.noreply.github.com>
2024-06-05 13:10:51 +0800

    initial

---
+++ personnel/src/test/java/org/example/personnel/PersonnelApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.personnel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonnelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
